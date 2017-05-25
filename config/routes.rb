20553f4e3f0a012e73fa47ad2ce26d88342c9134
Tashby <tashby2714@gmail.com>
2017-05-25 05:25:05 -0400

    comment functionality complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-resources :messages
+resources :messages do
+  resources :comments
+end
 root 'messages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
